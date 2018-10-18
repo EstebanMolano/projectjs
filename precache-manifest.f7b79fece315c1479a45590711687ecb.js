bc189c4a2c01c212b9fcd173f7b1f5ed6adc2d01
EstebanMolano <estebanmolano963@gmail.com>
2018-10-18 12:11:18 -0500

    Updates

---
+++ precache-manifest.f7b79fece315c1479a45590711687ecb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/proyectojs/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2b060f90aa65dd081864",
+    "url": "/proyectojs/static/js/runtime~main.2b060f90.js"
+  },
+  {
+    "revision": "f3741026c46e20a0d7d1",
+    "url": "/proyectojs/static/js/main.f3741026.chunk.js"
+  },
+  {
+    "revision": "f23b661b4bf19b8b584d",
+    "url": "/proyectojs/static/js/1.f23b661b.chunk.js"
+  },
+  {
+    "revision": "f3741026c46e20a0d7d1",
+    "url": "/proyectojs/static/css/main.1dd93a45.chunk.css"
+  },
+  {
+    "revision": "f23b661b4bf19b8b584d",
+    "url": "/proyectojs/static/css/1.af76aa62.chunk.css"
+  },
+  {
+    "revision": "77e136ce662c05e845f317e828e9beb0",
+    "url": "/proyectojs/index.html"
+  }
+];
